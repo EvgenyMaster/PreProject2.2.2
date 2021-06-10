76d1c2408efca31edcd4b33bc60c435ccd0fbc56
EvgenyMaster <evgenymaster6162@gmail.com>
2021-06-10 16:30:14 +0300

    ServiceRename

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -3,10 +3,9 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.Service.CarService;
-import web.Service.CarServiceImpl;
+import web.service.CarService;
+import web.service.CarServiceImpl;
 import web.model.Car;
 
 import java.util.ArrayList;
