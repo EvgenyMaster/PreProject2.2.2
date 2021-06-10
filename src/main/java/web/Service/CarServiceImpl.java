e1af636af0a208bebd1a308ee7b47a2602819965
EvgenyMaster <evgenymaster6162@gmail.com>
2021-06-10 16:28:27 +0300

    init

---
+++ src/main/java/web/Service/CarServiceImpl.java
@@ -0,0 +1,19 @@
+package web.Service;
+
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarServiceImpl implements CarService{
+    @Override
+    public List<Car> getCar(int count) {
+        List<Car> carList = new ArrayList<>();
+        carList.add(new Car(1, 100, "Audi"));
+        carList.add(new Car(2, 745, "BMW"));
+        carList.add(new Car(3, 2, "Volkswagen"));
+        carList.add(new Car(4, 2101, "Lada"));
+        carList.add(new Car(5, 140, "Mercedes"));
+        return carList.subList(0, (count >= 5 ? carList.size() : count));
+    }
+}
---
+++ src/main/java/web/controller/HelloController.java
@@ -0,0 +1,35 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.Service.CarService;
+import web.Service.CarServiceImpl;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class HelloController {
+
+    @GetMapping(value = "/")
+    public String printWelcome(ModelMap model) {
+        List<String> messages = new ArrayList<>();
+        messages.add("Hello!");
+        messages.add("I'm Spring MVC application");
+        messages.add("5.2.0 version by sep'19 ");
+        model.addAttribute("messages", messages);
+        return "index";
+    }
+
+    @GetMapping(value = "/cars")
+    public String printCars(ModelMap modelMap, @RequestParam(name = "count", required = false, defaultValue = "5") int count) {
+        CarService cars = new CarServiceImpl();
+        List<Car> messages = cars.getCar(count);
+        modelMap.addAttribute("carList", messages);
+        return "cars";
+    }
+}
